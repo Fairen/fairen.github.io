55baec07cc3a997944468103ea02c33ba0b7e8a0
unknown <mvacher@MVACHER.emea.cegedim.grp>
2015-08-28 16:24:29 +0200

    add parallax header and pictures

--- js/app.js
+++ js/app.js
@@ -1,3 +1,42 @@
+(function($) {
+   
+      $.fn.parallax = function(options) {
+   
+          var windowHeight = $(window).height();
+   
+          // Establish default settings
+          var settings = $.extend({
+              speed        : 0.15
+          }, options);
+   
+          // Iterate over each object in collection
+          return this.each( function() {
+   
+            // Save a reference to the element
+            var $this = $(this);
+   
+            // Set up Scroll Handler
+            $(document).scroll(function(){
+   
+                  var scrollTop = $(window).scrollTop();
+                        var offset = $this.offset().top;
+                        var height = $this.outerHeight();
+   
+            // Check if above or below viewport
+        if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
+          return;
+        }
+   
+        var yBgPosition = Math.round((offset - scrollTop) * settings.speed);
+   
+                          // Apply the Y Background Position to Set the Parallax Effect
+            $this.css('background-position', 'center ' + yBgPosition + 'px');
+                  
+            });
+          });
+      }
+  }(jQuery));
+
 $(function() {
  
     //Set up instafeed
@@ -8,11 +47,21 @@ $(function() {
         get: 'user',
         userId: 190514654,
         links: true,
-        limit: 4,
+        limit: 8,
         sortBy: 'most-recent',
         resolution: 'standard_resolution',
         template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="photo-box"><div class="image-wrap"><a href="{{link}}" target="_blank"><img src="{{image}}"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>'
     });
+
     feed.run();
+
+
+    $('header').parallax({
+        speed : .100
+    });
+
+    $('.parallax-container').parallax({
+        speed : .100
+    });
  
 });
